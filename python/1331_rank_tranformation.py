83ecb42bda86d45e8a99ffb9274792671d56d6bb
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2024-10-02 13:32:48 +0800

    24 Oct 02

---
+++ python/1331_rank_tranformation.py
@@ -0,0 +1,15 @@
+def arrayRankTransform(arr):
+    ranks = {i:0 for i in arr}
+    arr_sort = sorted(arr)
+    rank = 1
+
+    for j,k in enumerate(arr_sort):
+        ranks[k] = rank
+        if j!=len(arr_sort)-1 and arr_sort[j] != arr_sort[j+1]:
+            rank += 1
+    
+    return [ranks[r] for r in arr]
+        
+
+print(arrayRankTransform([40,10,20,30]))
+print(arrayRankTransform([100,100,100]))
